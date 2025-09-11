992605144d03fa78ac7412618a78a3986e13617f
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 22:46:06 +0330

    push the code

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ sudo apt-get install -y git ansible
 
 sudo rm -rf deploy-with-ansible
 
-git clone https://github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
+git clone https://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@github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
 
 sudo systemctl stop i_am_alive.service
 sudo systemctl disable i_am_alive.service

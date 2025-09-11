4f0b320078a8da019a1e4672e94e8028f577971e
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 21:28:11 +0330

    push the code

--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
-sudo apt-get update -y
-sudo apt-get upgrade -y
 sudo apt-get install -y git ansible
 
 git clone https://github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
+
+rm -rf /root/.ansible/cache/*
+
+rm -f *.retry
+
+rm -rf /tmp/ansible-*
 
 ansible-playbook ./deploy-with-ansible/ansible/deploy.yml

d1ac19c7c8a8fe27e0b900f050783a6e286b07f1
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 20:46:07 +0330

    push the code

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
-sudo apt install ansible -y
+sudo apt install git, ansible -y
 
 git clone https://github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
 

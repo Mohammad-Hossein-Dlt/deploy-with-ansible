1708bc42051fac9453d16ec7746c4615ba298020
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 21:53:01 +0330

    push the code

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sudo apt-get install -y git ansible
 
-git clone https://github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
+git clone https://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@github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
 
 rm -rf /root/.ansible/cache/*
 

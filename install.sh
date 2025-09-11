c918490baf7fd3919c7698386841fc407afa347c
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 22:36:51 +0330

    push the code

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 sudo apt-get install -y git ansible
 
-sudo rm ./deploy-with-ansible
+sudo rm -rf deploy-with-ansible
 
 git clone https://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@github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
 
 sudo systemctl stop i_am_alive.service
 sudo systemctl disable i_am_alive.service
 
-sudo rm /etc/systemd/system/i_am_alive.service
-sudo rm /opt/deploy-with-ansible/i_am_alive.py
+sudo rm -rf /etc/systemd/system/i_am_alive.service
+sudo rm -rf /opt/deploy-with-ansible/i_am_alive.py
 sudo systemctl daemon-reload
 sudo systemctl reset-failed
 

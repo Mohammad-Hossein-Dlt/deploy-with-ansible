6268a488f16299b0e39edd91c134ddc180bd55a7
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 22:18:12 +0330

    push the code

--- install.sh
+++ install.sh
@@ -2,10 +2,11 @@ sudo apt-get install -y git ansible
 
 git clone https://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@github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
 
-rm -rf /root/.ansible/cache/*
+sudo systemctl stop i_am_alive.service
+sudo systemctl disable i_am_alive.service
 
-rm -f *.retry
-
-rm -rf /tmp/ansible-*
+sudo rm /etc/systemd/system/i_am_alive.service
+sudo systemctl daemon-reload
+sudo systemctl reset-failed
 
 ansible-playbook ./deploy-with-ansible/ansible/deploy.yml

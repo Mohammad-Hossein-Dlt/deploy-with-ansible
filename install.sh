1125eb1019ae1095be1dd1f82e7a78fc16414abb
Mohammad_Hosein.Dlt <hosein27d@gmail.com>
2025-09-11 21:02:56 +0330

    push the code

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sudo apt-get install -y git ansible
 
 git clone https://github.com/Mohammad-Hossein-Dlt/deploy-with-ansible.git
 
-ansible-playbook ./ansible/deploy.yml
+ansible-playbook ./deploy-with-ansible/ansible/deploy.yml
